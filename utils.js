c844e96e1a7b4b0872747ef27905ea1029442fdc
PINGWINbl4 <mikmez01@gmail.com>
2023-09-26 09:23:19 +0500

    logging for fix

--- utils.js
+++ utils.js
@@ -52,7 +52,7 @@ async function findShelduesOfStation(gatewayId){
     shelldues.push(await db.shelldue.findFirst({
       where:{
           id: shellduesId[i].shelldueId,
-          shelldueType: "condition"
+          shelldueType: "tip"
       }
     }))
   }
