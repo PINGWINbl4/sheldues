9e9213fcde0d614f7eee0d1195c7da0c41197537
PINGWINbl4 <mikmez01@gmail.com>
2023-09-26 09:34:47 +0500

    logging for fix

--- utils.js
+++ utils.js
@@ -48,6 +48,7 @@ async function findShelduesOfStation(gatewayId){
   if(!shellduesId.length){
     throw new Error(`Station with ${gatewayId} gatevayId haven't sheldues`)
   }
+  console.log(shellduesId)
   for (let i = 0; i < shellduesId.length; i++) {
     shelldues.push(await db.shelldue.findFirst({
       where:{
@@ -56,7 +57,6 @@ async function findShelduesOfStation(gatewayId){
       }
     }))
   }
-  await Promise.all()
   return shelldues
 }
 
