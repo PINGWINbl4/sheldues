ad5f09062039435c7b9a7ed6dfc834ec87d9a1b8
PINGWINbl4 <mikmez01@gmail.com>
2023-10-27 12:46:29 +0500

    lkm

--- mqttMessageController/massageController.js
+++ mqttMessageController/massageController.js
@@ -9,6 +9,7 @@ async function getMQTTData(topic, payload){
     try{
         topic = parseTopic(topic)
         getSend = JSON.parse(stringPayload);
+        console.log(getSend)
         const user = await utils.findUser(topic.userId)
         const stationsShelldues = await utils.findShelduesOfStation(topic.gatewayId)
         const station = await utils.findStationAtDB(topic.gatewayId)
