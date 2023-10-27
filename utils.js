8db330104df531b4e82e347201d8541824d88b88
PINGWINbl4 <mikmez01@gmail.com>
2023-10-27 11:25:16 +0500

    jkn

--- utils.js
+++ utils.js
@@ -61,8 +61,9 @@ async function findShelduesOfStation(gatewayId){
     })
     console.log(shelldue.active && (!shelldue.activeDays.length || (shelldue.activeDays.length && shelldue.activeDays.includes(date.getDay()))))
     shelldue.active && (!shelldue.activeDays.length ||  shelldue.activeDays.includes(date.getDay())) 
-    ? shelldues.push(shelldue):console.log(`shalldue ${shelldue.id} non active in this day \n${shelldue}`)
+    ? shelldues.push(shelldue):console.log(`shalldue ${shelldue.id} non active in this day`)
   }
+  console.log(shelldues)
   return shelldues
 }
 
